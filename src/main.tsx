b24c010e445481f192babc8118ac56af2c8dfcc4
simoderyouch <simoederyouch@outlook.com>
2026-02-23 02:02:06 +0000

    add analysis

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import './index.css'
 import './i18n'
 import App from './App.tsx'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
